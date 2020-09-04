a0335e5efb0ad5a8b548db7fb4dcaaba6e8c7317
Brian Henderson <henderson.brian82@gmail.com>
2020-09-04 11:56:42 -0400

    Added a Temperature Converter

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TempConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's convert a temperature form Fahrenheit to Celsius, then vice-versa");
+            Console.WriteLine("First, we'll convert a Fahrenheit temperature:");
+            var celsius = TempConverter.FahrenheitToCelsius(78);
+            Console.WriteLine($"After conversion that comes out to {celsius} degrees C");
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("Now we'll convert a celsius temperature:");
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(18);
+            Console.WriteLine($"This comes out to {fahrenheit} degrees F");
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/TempConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TempConverter
+{
+    static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double fahrenheit)
+        {
+            var resultOne = (fahrenheit - 32) / 1.8;
+            return resultOne;
+        }
+    
+        public static double CelsiusToFahrenheit(double celsius)
+        {
+            var resultTwo =  (celsius * 9) / 5 + 32;
+            return resultTwo;
+        }
+    }
+}
